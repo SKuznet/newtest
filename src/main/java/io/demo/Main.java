1f2248594bf63fbe33eecfe3ace94d8c1f2f8339
skuznet <azon.sk@gmail.com>
2018-05-11 20:16:51 +0300

    add additional functionality to Main

--- src/main/java/io/demo/Main.java
+++ src/main/java/io/demo/Main.java
@@ -2,6 +2,8 @@ package io.demo;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Starting...");
         System.out.println("Hello world!");
+        System.out.println("End!");
     }
 }
